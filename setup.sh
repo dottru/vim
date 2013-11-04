064dd9b6eb82c1a5e84da7926c3c0dbdf15dae56
Joshua Kogut <dottru@gmail.com>
2013-11-03 22:59:24 -0500

    vim setup testing

--- setup.sh
+++ setup.sh
@@ -1,39 +1,38 @@
 #!/usr/bin/env bash
+
 . lib/txt.sh;
 . lib/control.sh
 . lib/git.sh
-############# libs #
 
+VUNDLE="https://github.com/gmarik/vundle.git";
+
+############# libs #
 # TODO: re-enable
 # Msg "Installing ctags";
 # sudo apt-get install ctags;
+####################
 
 Msg "Changing directory to vim dotfiles.";
-pushd "${HOME}/.vim/";
+pushd "${HOME}/.vim";
 
-VUNDLE="https://github.com/gmarik/vundle.git";
 Msg "Cloning vundle into bundle/vundle...";
-
-# Clone vundle into fresh dir
 rm -rf bundle/vundle; git clone $VUNDLE bundle/vundle;
 
-# Run setup
-Msg "Installing bundles.";
+Msg "Installing vundle packages.";
 vim +BundleInstall +qall;
+Pause;
 
 # These come in renamed to avoid errors during installation
 if [ -d "_plugin" ]; then
+    # TODO: remove
     echo "Renamed plugins."; Pause;
     mv _plugin plugin; 
 fi;
 
 # Build YCM
-pushd 'bundle/YouCompleteMe';
-Msg "Install: cmake python-dev\n\n    before pressing [Enter].";
-Pause;
-./install.sh;
-popd;
+pushd "bundle/YouCompleteMe";
+Msg "Install: cmake python-dev\n\n    before pressing [Enter]."; Pause;
+./install.sh; popd;
 
 Msg "VIM configuration completed.";
-Pause;
-popd;
+Pause; popd;
