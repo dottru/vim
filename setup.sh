6f6fbe46d529af7f74aee59c83502c1d33ba8f1b
Checkers Michael <dottru@gmail.com>
2013-11-03 16:01:11 -0800

    fixed vim setup bug

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-
-# libs ############
-
 . lib/txt.sh;
 . lib/control.sh
 . lib/git.sh
+############# libs #
 
-###################
 
 Msg "Installing ctags";
 sudo apt-get install ctags;
@@ -14,32 +11,10 @@ sudo apt-get install ctags;
 Msg "Changing directory to vim dotfiles.";
 DirPush "~/.vim";
 
-# clone vundle
 VUNDLE="https://github.com/gmarik/vundle.git";
 Msg "Cloning vundle into bundle/vundle...";
 rm -rf bundle/vundle;
 git clone $VUNDLE bundle/vundle;
 
-#echo "Updating git submodules.";
-#GitInitSubmodules;
-#NL;
-
-# vim variables
-VI_CFG="${HOME}/.vim";
-VIM=`pwd`;
-
-Msg " VIM directories ------------------- ";
-Msg "     dotfiles - `pwd`";
-Msg "     cfg      - ${VI_CFG}"; NL;
-
-LinkBack "${VIM}" "${VI_CFG}";
-LinkBack "${VIM}/vimrc" "${HOME}/.vimrc";
-
-DirPop;
-
-echo "VIM configuration sucessful.";
-echo "Launching vundle, VIM's ackage manager...";
-Pause;
-
+Msg "Installing bundles.";
 vim +BundleInstall +qall;
-Pause; NL;
